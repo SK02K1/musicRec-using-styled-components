485f784b9d9d6cae8b5a439bfdc01ae5797efdba
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2021-10-10 15:27:08 +0530

    style: add theme provider - added theme data - added state variable for theme provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { GlobalStyles } from "./components/styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import { darkMode, lightMode } from "./data/themes.js";
+import { useState } from "react";
 
 export default function App() {
+  const [appTheme, setAppTheme] = useState(lightMode);
   return (
     <div className="App">
-      <GlobalStyles />
+      <ThemeProvider theme={appTheme}>
+        <GlobalStyles />
+      </ThemeProvider>
     </div>
   );
 }
--- src/components/styles/GlobalStyles.jsx
+++ src/components/styles/GlobalStyles.jsx
@@ -4,6 +4,16 @@ export const GlobalStyles = createGlobalStyle`
 
 :root {
   --manrope: Manrope, Arial, Helvetica, sans-serif;
+  --bg-color: ${({
+    theme: {
+      colors: { bg }
+    }
+  }) => bg};
+  --text-color: ${({
+    theme: {
+      colors: { primaryText }
+    }
+  }) => primaryText};
 }
 
 @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700&display=swap');
@@ -18,10 +28,13 @@ body {
   min-height: 100vh;
   font-family: var(--manrope);
   font-weight: 400;
+  background-color: var(--bg-color);
+  color: var(--text-color);
 }
 
 a {
   text-decoration: none;
+  color: var(--text-color);
 }
 
 button {
