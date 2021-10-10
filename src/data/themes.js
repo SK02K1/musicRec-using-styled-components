1bdfedaae19f047f1b367309a43f9c91f96a416b
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2021-10-10 13:47:21 +0530

    add: theme data

---
+++ src/data/themes.js
@@ -0,0 +1,16 @@
+export const darkMode = {
+  colors: {
+    bg: "#2f3136",
+    card: "#36393F",
+    primaryText: "#FFFFFF",
+    secondaryText: "#DCDDDE"
+  }
+};
+export const lightMode = {
+  colors: {
+    bg: "#FFFFFF",
+    card: "#DCDDDE",
+    primaryText: "#2f3136",
+    secondaryText: "#36393F"
+  }
+};
