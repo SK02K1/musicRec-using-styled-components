705ac03f7a0a8e9ae527476e1262f6749a082a0e
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2021-10-10 18:24:14 +0530

    add: Music DB

---
+++ src/data/MusicDB.js
@@ -0,0 +1,50 @@
+export const getMusicDB = () => {
+  return {
+    Punjabi: [
+      {
+        name: "Brown Munde",
+        artist: "AP Dhillon, Gminxr, Gurinder",
+        albumArt:
+          "https://i.scdn.co/image/ab67616d0000b273d9a129c4a656a55afff2ca02",
+        videoLink: "https://www.youtube.com/watch?v=VNs_cCtdbPc"
+      },
+
+      {
+        name: "Hukum",
+        artist: "Karan Aujla",
+        albumArt:
+          "https://i.scdn.co/image/ab67616d0000b273634a1dfb2496a400b3d010b7"
+      },
+
+      {
+        name: "Goat",
+        artist: "Sidhu Moose Wala",
+        albumArt:
+          "https://i.scdn.co/image/ab67616d0000b273ce9b55302f3fa4411499553d"
+      }
+    ],
+
+    Romance: [
+      {
+        name: "Kaise Hua",
+        artist: "Vishal Mishra",
+        albumArt:
+          "https://i.scdn.co/image/ab67616d0000b273c5545f737b16ad5ee767b62a"
+      },
+
+      {
+        name: "Dil Diyan Gallan",
+        artist: "Vishal Shekhar, Julius Packiam, Atif Aslam",
+        albumArt:
+          "https://i.scdn.co/image/ab67616d0000b273576521b1bf3ec2fd7fdfcbd5"
+      },
+
+      {
+        name: "Tum Se Hi",
+        artist: "Pritam, Mohit Chauhan",
+        albumArt:
+          "https://i.scdn.co/image/ab67616d0000b27352fe6875028c892308ffc2f7"
+      }
+    ]
+  };
+};
